0ff33631cfcdffa95fc7bb93782db2943e9a4f39
Przemek  Ptasznik99 <przemek_nowocin@interia.pl>
2017-11-28 10:26:12 +0100

    Dodanie xunit

--- ClassZadanie1/ClassZadanie1/Class1.cs
+++ ClassZadanie1/ClassZadanie1/Class1.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace ClassZadanie1
 {
-    public class Class1
+    public class Matematyka
     {
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public double Subtract (double x, double y)
+        {
+            return x - y;
+        }
+
+        public double Multiplaction(double x, double y)
+        {
+            return x * y;
+        }
+
+        public double Division (double x, double y)
+        {
+            return x / y;
+        }
     }
 }
